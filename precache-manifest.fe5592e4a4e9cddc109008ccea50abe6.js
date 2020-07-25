06c7d9562ff9d34e9902453fd55bb6570cfd3516
linzheng <alidlg9045@126.com>
2020-07-25 13:18:15 +0800

    deploy

---
+++ precache-manifest.fe5592e4a4e9cddc109008ccea50abe6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c91b135878ad4ecf24e",
+    "url": "/money-1-Vue-website/css/app.9be5bee0.css"
+  },
+  {
+    "revision": "70198d277db5ed9d5e0e1efc9e67b4db",
+    "url": "/money-1-Vue-website/index.html"
+  },
+  {
+    "revision": "1c91b135878ad4ecf24e",
+    "url": "/money-1-Vue-website/js/app.7e8bf822.js"
+  },
+  {
+    "revision": "a6d72df05efdae5cedf2",
+    "url": "/money-1-Vue-website/js/chunk-vendors.cd2003bc.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/money-1-Vue-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-1-Vue-website/robots.txt"
+  }
+]);
